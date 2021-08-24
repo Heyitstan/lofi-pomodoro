c1a71bea110e3e6cdb6117064dd0130c6363108a
Heyitstan <heyitstan@gmail.com>
2021-08-24 13:10:54 -0700

    added background image with preloaded gif

---
+++ src/Components/giphy.js
@@ -0,0 +1,15 @@
+var gif = "https://media3.giphy.com/media/RMwgs5kZqkRyhF24KK/giphy.gif?cid=ecf05e47ala4tra0quxj45tgqs3j07uey1ren7vnkfw0p6xf&rid=giphy.gif&ct=g";
+
+window.onload = function () {
+  var myDiv = document.getElementById('bg'); // grabs #my-div
+  var myPath = gif; // initialises string variable myPath
+  var myImg = document.createElement('img'); // creates a new img element
+
+  // add attributes to myImg
+  myImg.setAttribute('src', myPath);
+  myImg.setAttribute('id', 'background');
+  myImg.setAttribute('alt', "background image"); 
+  
+  myDiv.appendChild(myImg); // adds a child element (myImg) to myDiv
+}
+
