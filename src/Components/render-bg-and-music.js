1953e79e2971806aeb23066c8c8dc77c6988239a
Nina Tan <ninavtan@gmail.com>
2021-08-24 17:20:17 -0600

    added timer functionality and ui

--- src/Components/render-bg-and-music.js
+++ src/Components/render-bg-and-music.js
@@ -1,7 +1,9 @@
 import musicPlayer from "./soundcloud-player";
 import background from './giphy'
+import timer from './timer';
 
 window.onload = function () {
   background();
   musicPlayer();
+  timer();
 }
