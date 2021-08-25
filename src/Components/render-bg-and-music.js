ba495d333043060b946df382387603f02191daa4
Heyitstan <heyitstan@gmail.com>
2021-08-25 00:55:08 -0700

    added a spotify player, but also changed a lot of code to use react

--- src/Components/render-bg-and-music.js
+++ src/Components/render-bg-and-music.js
@@ -1,9 +1,11 @@
-import musicPlayer from "./soundcloud-player";
-import background from './giphy'
+// import musicPlayer from "./soundcloud-player";
+// import background from './giphy'
 import timer from './timer';
+// import spotifyPlayer from "./spotify";
 
 window.onload = function () {
-  background();
-  musicPlayer();
+//   background();
+//   musicPlayer();
+//   spotifyPlayer();
   timer();
 }
