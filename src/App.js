1422bfde95428b178cd9bcde2a785506d0a091b5
Heyitstan <heyitstan@gmail.com>
2021-08-24 18:30:13 -0700

    added button to change background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import './Components/render-bg-and-music'
+import generateNewBackground from "./Components/new-background-button";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <div id="bg">
       </div>
 
-
+      <button id="random-background-btn" type="button" className="btn" onClick={generateNewBackground}>New background</button>
 
     </div> 
   );
--- src/Components/giphy.js
+++ src/Components/giphy.js
@@ -1,4 +1,16 @@
-var gif = "https://media3.giphy.com/media/RMwgs5kZqkRyhF24KK/giphy.gif?cid=ecf05e47ala4tra0quxj45tgqs3j07uey1ren7vnkfw0p6xf&rid=giphy.gif&ct=g";
+var gif = "https://media1.giphy.com/media/RMwgs5kZqkRyhF24KK/giphy.gif?%E2%80%A68b9cigz8i9zhf17rkkqyxckwp7j7kd4xhyppecpw0&rid=giphy.gif&ct=g";
+
+var gifList = []
+
+var generateList = (data) => {
+  for( let i = 0; i < 15; i++) {
+    gifList.push(data.data.[i].images.original.url)
+  };
+}
+
+fetch("https://api.giphy.com/v1/gifs/search?api_key=fw59qRGgKMWBeCqRy39YOomgcWI3iRWq&q=lofi+anime&limit=15&offset=0&rating=g&lang=en")
+  .then(response => response.json())
+  .then(data => generateList(data))
 
 var background = function () {
   var myDiv = document.getElementById('bg'); // grabs #bg
@@ -13,4 +25,5 @@ var background = function () {
   myDiv.appendChild(myImg); // adds a child element (myImg) to myDiv
 }
 
+export {gifList}
 export default background
