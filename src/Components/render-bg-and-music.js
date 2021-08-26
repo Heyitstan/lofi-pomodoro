2bbbd28f9692301d5f0715a4fceb63577cfcca98
Nina Tan <ninavtan@gmail.com>
2021-08-25 18:04:05 -0600

    created two different components for timer-buttons and timer-display

--- src/Components/render-bg-and-music.js
+++ src/Components/render-bg-and-music.js
@@ -1,6 +1,6 @@
 // import musicPlayer from "./soundcloud-player";
 // import background from './giphy'
-import timer from './timer';
+import timer from './timer-buttons';
 // import spotifyPlayer from "./spotify";
 
 // window.onload = function () {
