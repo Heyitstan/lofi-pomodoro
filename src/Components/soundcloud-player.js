49c855f51010b044e13d6243b0a05891ac9d9e1b
Heyitstan <heyitstan@gmail.com>
2021-08-26 15:04:52 -0700

    fixed volume error, now the rain and piano start soft

--- src/Components/soundcloud-player.js
+++ src/Components/soundcloud-player.js
@@ -1,17 +1,29 @@
 import piano from './mp3s/Piano-music.mp3'
+import { useEffect } from 'react';
+
 
 var music = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/325736605&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=&visual=true";
 
 var MusicPlayer = function () {
+    // useEffect runs the inner function after render
+    useEffect(() => {
+      const rain = document.getElementById('audio-rain');
+      const piano = document.getElementById('audio-piano')
+      // If audioNode isn't found, do nothing
+      if (!rain || !piano) {
+        return;
+      }
+  
+      piano.volume = 0.2;
+      rain.volume = 0.2;
+    }, []); // Empty array here means "only run it once on page load"
   return (
     <div>
     {/* <iframe src= {music} title="music" id="music-player" width="220" height="100" scrolling="no" frameBorder="yes" /> */}
     <p id='piano-title'>Relaxing Piano</p>
     <audio id="audio-piano" controls controlsList='nodownload' loop='true' src={piano} type='audio/mpeg' />
-    {document.getElementById('audio-piano').volume=0.2}
     <p id='rain-title'>Rain</p>
     <audio id="audio-rain" controls controlsList='nodownload' loop='true' src="https://www.soundjay.com/nature/sounds/rain-07.mp3" type='audio/mpeg' />
-    {document.getElementById('audio-rain').volume=0.2}
     </div>
   )
 }
